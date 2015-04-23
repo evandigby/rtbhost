201b780f84c84080b97172a6328d3ab898e4eb7b
evandigby <evandigby@gmail.com>
2015-04-22 21:14:23 -0700

    Import

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"os"
+)
+
+func main() {
+	var address string
+	var endpoint string
+	var options RtbAppOptions
+	var logToFile bool
+	var fileName string
+
+	flag.StringVar(&address, "address", "localhost:8000", "Address of this instance, including port.")
+	flag.StringVar(&endpoint, "endpoint", "/", "Endpoint for.")
+	flag.StringVar(&options.Domain, "appdomain", "rtb-interview", "App domain used to uniquely identify this application. All server instances of the same application should match.")
+	flag.StringVar(&options.DataAccessOptions.Network, "redisnetwork", "tcp", "Redis network.")
+	flag.StringVar(&options.DataAccessOptions.Address, "redisaddress", "localhost:6379", "Redis network.")
+	flag.IntVar(&options.DataAccessOptions.ConnectionPoolSize, "redispoolsize", 100, "Maximum number of concurrent redis connections from this host.")
+	flag.StringVar(&options.LoggingOptions.AmqpAddress, "amqpaddress", "amqp://guest:guest@localhost:5672/", "AMQP Logging address.")
+	flag.BoolVar(&logToFile, "logtofile", true, "Log to file (overrides amqp options).")
+	flag.StringVar(&fileName, "filename", "", "File to log to (defaults to stdout).")
+	flag.BoolVar(&options.LoggingOptions.Verbose, "logverbose", false, "Log each request.")
+	flag.BoolVar(&options.LoggingOptions.FullBidResponse, "logfullbidresponse", false, "Log full bid response (json).")
+	flag.BoolVar(&options.PeriodicUpdates, "periodicupdates", true, "Print periodic updates on campaigns to screen.")
+	flag.IntVar(&options.UpdateFrequencyInSeconds, "updatefrequency", 5, "Update period in seconds.")
+
+	flag.Parse()
+
+	if logToFile {
+		options.LoggingOptions.AmqpAddress = ""
+		if fileName == "" {
+			options.LoggingOptions.File = os.Stdout
+		} else {
+			file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+
+			if err == nil {
+				defer file.Close()
+				options.LoggingOptions.File = file
+			}
+		}
+	}
+
+	app := NewRtbApp(options)
+
+	http.HandleFunc(endpoint, app.BidRequestHandler)
+	http.ListenAndServe(address, nil)
+}
---
+++ rtbapp.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/evandigby/rtb"
+	"github.com/evandigby/rtb/amqp"
+	"github.com/evandigby/rtb/inmemory"
+	rtbr "github.com/evandigby/rtb/redis"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+// Temporary in memory state for testing
+type RtbApp struct {
+	logger           rtb.BidLogProducer
+	bidLoggerChannel chan *rtb.BidLogItem // Limits the number of connections to loggers
+
+	domain       string
+	redisNetwork string
+	redisAddress string
+
+	campaignProvider rtb.CampaignProvider
+	banker           rtb.Banker
+	pacer            rtb.Pacer
+	updateTicker     *time.Ticker
+	accountBids      chan *rtb.BidResponse
+	bidTotals        map[int64]int64
+}
+
+type RtbAppOptions struct {
+	Domain                   string
+	PeriodicUpdates          bool
+	UpdateFrequencyInSeconds int
+	DataAccessOptions        struct {
+		Network            string
+		Address            string
+		ConnectionPoolSize int
+	}
+	LoggingOptions struct {
+		AmqpAddress     string
+		File            *os.File
+		Verbose         bool
+		FullBidResponse bool
+	}
+}
+
+func (app *RtbApp) CreateDefaultCampaigns() {
+	app.campaignProvider.CreateCampaign(100101, rtb.CpmToMicroCents(0.32), rtb.DollarsToMicroCents(35.50), []rtb.Target{rtb.Target{Type: rtb.Placement, Value: "Words With Friends 2 iPad"}})
+	app.campaignProvider.CreateCampaign(100102, rtb.CpmToMicroCents(0.04), rtb.DollarsToMicroCents(5.25), []rtb.Target{rtb.Target{Type: rtb.CreativeSize, Value: "728x90"}})
+	app.campaignProvider.CreateCampaign(100103, rtb.CpmToMicroCents(0.32), rtb.DollarsToMicroCents(15.00), []rtb.Target{rtb.Target{Type: rtb.Country, Value: "USA"}})
+	app.campaignProvider.CreateCampaign(100104, rtb.CpmToMicroCents(0.15), rtb.DollarsToMicroCents(22.00), []rtb.Target{rtb.Target{Type: rtb.OS, Value: "Android"}})
+	app.campaignProvider.CreateCampaign(100105, rtb.CpmToMicroCents(0.02), rtb.DollarsToMicroCents(2.25), []rtb.Target{rtb.Target{Type: rtb.Country, Value: "CAN"}})
+}
+
+func (app *RtbApp) bidRequestForHttpRequest(r *http.Request) *rtb.BidRequest {
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var bidRequest rtb.BidRequest
+
+	err = json.Unmarshal(body, &bidRequest)
+
+	return &bidRequest
+}
+
+func (app *RtbApp) BidRequestHandler(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now().UTC().UnixNano()
+	bidRequest := app.bidRequestForHttpRequest(r)
+
+	bidder := inmemory.NewBidRequestBidder(bidRequest, app.campaignProvider, app.pacer, time.Now().UTC())
+
+	b, remainingDailyBudgetsInMicroCents, err := bidder.Bid()
+
+	if err == nil && b != nil {
+		js, err := json.Marshal(b)
+
+		if err == nil {
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(js)
+
+			// Push the bid onto our stats process
+			app.accountBids <- b
+		} else {
+			w.WriteHeader(http.StatusNoContent)
+		}
+	} else {
+		w.WriteHeader(http.StatusNoContent)
+	}
+
+	if app.logger != nil {
+		app.bidLoggerChannel <- &rtb.BidLogItem{Domain: app.domain, BidRequest: bidRequest, BidResponse: b, RemainingDailyBudgetsInMicroCents: remainingDailyBudgetsInMicroCents, StartTimestampInNanoseconds: startTime, EndTimestampInNanoseconds: time.Now().UTC().UnixNano()}
+	}
+
+}
+
+func (app *RtbApp) PrintCampaigns(campaigns []int64) {
+	fmt.Println()
+	for _, val := range campaigns {
+		c := app.campaignProvider.ReadCampaign(val)
+		remaining := app.banker.RemainingDailyBudgetInMicroCents(c.Id())
+		fmt.Printf("Campaign Id: %v / CPM: $%v / Remaining Daily Budget: $%v / Total Bids: %v\n", c.Id(), rtb.MicroCentsToDollarsRounded(c.BidCpmInMicroCents(), 2), rtb.MicroCentsToDollarsRounded(remaining, 5), app.bidTotals[c.Id()])
+	}
+}
+
+func (app *RtbApp) PrintUpdates() {
+	for {
+		<-app.updateTicker.C
+		app.PrintCampaigns(app.campaignProvider.ListCampaigns())
+	}
+}
+
+func (app *RtbApp) BidTotalUpdater() {
+	for {
+		rsp := <-app.accountBids
+
+		if rsp.Seatbid != nil {
+			for _, rsp := range rsp.Seatbid {
+				if rsp.Bid != nil {
+					for _, bid := range rsp.Bid {
+						campaignId, _ := strconv.ParseInt(bid.Cid, 10, 64)
+						total := app.bidTotals[campaignId] + 1
+						app.bidTotals[campaignId] = total
+					}
+				}
+			}
+		}
+	}
+}
+
+func (app *RtbApp) LogBids() {
+	for {
+		logItem := <-app.bidLoggerChannel
+
+		app.logger.LogItem(logItem)
+	}
+}
+
+func NewRtbApp(options RtbAppOptions) *RtbApp {
+	app := new(RtbApp)
+
+	app.domain = options.Domain
+	app.redisAddress = options.DataAccessOptions.Address
+	app.redisNetwork = options.DataAccessOptions.Network
+
+	if options.LoggingOptions.Verbose || options.LoggingOptions.FullBidResponse {
+		if options.LoggingOptions.File != nil {
+			app.logger = inmemory.NewFileBidLogger(options.LoggingOptions.File, options.LoggingOptions.FullBidResponse)
+		} else {
+			app.logger = amqp.NewAmqpBidLogger(options.LoggingOptions.AmqpAddress, options.Domain)
+		}
+
+		app.bidLoggerChannel = make(chan *rtb.BidLogItem)
+		go app.LogBids()
+	}
+
+	da := rtbr.NewRedisDataAccess(app.redisNetwork, app.redisAddress, app.domain, options.DataAccessOptions.ConnectionPoolSize)
+	app.banker = rtbr.NewRedisBanker(da)
+
+	app.campaignProvider = rtbr.NewRedisCampaignProvider(da, app.banker)
+	app.pacer = rtbr.NewRedisPacer(da, app.banker)
+	app.CreateDefaultCampaigns()
+
+	if options.PeriodicUpdates {
+		app.updateTicker = time.NewTicker(time.Duration(options.UpdateFrequencyInSeconds) * time.Second)
+
+		go app.BidTotalUpdater()
+		go app.PrintUpdates()
+	}
+
+	app.accountBids = make(chan *rtb.BidResponse)
+	app.bidTotals = make(map[int64]int64)
+
+	return app
+}
